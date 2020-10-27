da544cbafc948f3f11538126c3be8da282c3a308
gigioSouza <giovane.bds@gmail.com>
2020-10-27 00:55:14 +0000

    Deploying to gh-pages from  @ 61702201f0aedb19b389e42c95df20c5f685567f 🚀

---
+++ _nuxt/c5dfc31.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{266:function(t,n,e){"use strict";e.r(n);var l={name:"Index"},o=e(48),component=Object(o.a)(l,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"container"},[n("div",[n("nuxt-link",{attrs:{to:"/blog"}},[this._v("Blog")])],1)])}),[],!1,null,null,null);n.default=component.exports}}]);
