ad676b357763c19b06f575f43229ccb616df27d8
Giovane Souza <giovane.bds@gmail.com>
2021-02-26 00:19:41 -0300

    Fixed FormatDate function and refactored cors-error-api-call article in pt

--- utils/locale.js
+++ utils/locale.js
@@ -2,10 +2,10 @@ export function ResolveContentLocale(content, i18n) {
   return content + '/' + i18n.locale;
 }
 
-export function FormatDate(date, i18n) {
-  let iso = 'en-US';
-  if (i18n.locale === 'pt') {
-    iso = 'pt-BR';
+export function FormatDate(date, locale) {
+  let iso = 'en-us';
+  if (locale === 'pt') {
+    iso = 'pt-br';
   }
   return new Intl.DateTimeFormat(iso).format(new Date(date));
 }
